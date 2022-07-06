44b0591e1d68d58e7ec24b33166a761eb0b328c4
Anton Makarov <makarov070786@mail.ru>
2022-07-06 17:12:10 +0300

    first views

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -9,13 +9,62 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        HStack(spacing: 20) {
+            ForEach(0..<4) {_ in
+                CardView(isFaceUp: <#Bool#>)
+            }
+        }
+        .foregroundColor(.orange)
+        .padding()
+        .font(.largeTitle)
+    }
+
+    struct CardView: View {
+        var isFaceUp: Bool 
+        var body: some View {
+            ZStack {
+                if isFaceUp {
+                    RoundedRectangle(cornerRadius: 10).fill(.white)
+                    RoundedRectangle(cornerRadius: 10).stroke(lineWidth: 3)
+                    Text("🐶")
+                } else {
+                    RoundedRectangle(cornerRadius: 10).fill()
+                }
+            }
+        }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .padding(0.0)
     }
 }
